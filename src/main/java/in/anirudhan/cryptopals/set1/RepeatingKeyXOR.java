8a23b0d9e163e444c49c3cad0eb0b9031071c9e1
Anirudhan J. Rajagopalan <ios@anirudhan.in>
2020-09-27 10:58:37 -0700

    Minor edits

--- src/main/java/in/anirudhan/cryptopals/set1/RepeatingKeyXOR.java
+++ src/main/java/in/anirudhan/cryptopals/set1/RepeatingKeyXOR.java
@@ -10,7 +10,6 @@ public class RepeatingKeyXOR {
         String output = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272" +
                 "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
 
-
         String key = "ICE";
 
         String encrypted = encrypt(inputString, key);
@@ -21,11 +20,8 @@ public class RepeatingKeyXOR {
         for(int i =0; i < encrypted.length(); i++){
             if(encrypted.charAt(i) == output.charAt(i)){continue;}else{System.out.println("Not equal");break;}
         }
-
+        System.out.println(encrypted);
         System.out.println("All good");
-//        System.out.println(encrypted);
-
-
     }
 
     private static String encrypt(String inputString, String key) {
@@ -33,7 +29,6 @@ public class RepeatingKeyXOR {
         for(int i=0; i < inBytes.length; i++){
             inBytes[i] = (byte) (inBytes[i] ^ key.charAt(i%key.length()));
         }
-
         return String.valueOf(Hex.encodeHex(inBytes));
     }
 
